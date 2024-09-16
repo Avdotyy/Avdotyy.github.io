3d5580c5331c9e8fc8d5ab9963836f4075354dce
Avdotyy <yura.bashlykov1321@gmail.com>
2024-09-16 22:13:14 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,1 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
 
-/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	alert('https://vk.com/video-127960182_456239022')
-})
